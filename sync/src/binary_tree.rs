69acefb72fe30151c6ba9076535be9a919a4bbf6
OscartGiles <oscart.giles@gmail.com>
2024-09-05 10:32:00 +0100

    Implement AVL Tree

--- sync/src/binary_tree.rs
+++ sync/src/binary_tree.rs
@@ -1,43 +1,128 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{f64::consts::PI, fmt::Debug};
 
-type NodeRef<V> = Box<RefCell<Node<V>>>;
+type NodeRef<V> = Box<Node<V>>;
 
 #[derive(Debug)]
 struct Node<V> {
     left: Option<NodeRef<V>>,
     right: Option<NodeRef<V>>,
+    height: usize,
     value: V,
 }
 
 impl<V: std::cmp::PartialOrd> Node<V> {
     pub fn new(value: V) -> Node<V> {
-        Self {
+        Node {
             left: None,
             right: None,
+            height: 1,
             value,
         }
     }
 
-    fn insert(&mut self, value: V) {
-        if value < self.value {
-            match &self.left {
-                Some(left_node) => left_node.borrow_mut().insert(value),
-                None => self.left = Some(Box::new(RefCell::new(Node::new(value)))),
+    fn height(node: &Option<NodeRef<V>>) -> usize {
+        node.as_ref().map_or(0, |n| n.height)
+    }
+    fn right_height(&self) -> usize {
+        self.right.as_ref().map_or(0, |n| n.height)
+    }
+}
+
+impl<V: std::cmp::PartialOrd> Node<V> {
+    fn rotate_right(mut self: NodeRef<V>) -> NodeRef<V> {
+        let mut left_child = self.left.take().unwrap();
+        let left_child_right = left_child.right.take();
+
+        self.fix_height();
+        left_child.right = Some(self);
+
+        if let Some(node) = &mut left_child.right {
+            node.left = left_child_right;
+        }
+        left_child.fix_height();
+        left_child
+    }
+
+    fn rotate_left(mut self: NodeRef<V>) -> NodeRef<V> {
+        let mut right_child = self.right.take().unwrap();
+        let right_child_left = right_child.left.take();
+
+        self.fix_height();
+        right_child.left = Some(self);
+
+        if let Some(node) = &mut right_child.left {
+            node.right = right_child_left;
+        }
+        right_child.fix_height();
+        right_child
+    }
+
+    fn fix_height(&mut self) {
+        let left_height = Node::height(&self.left);
+        let right_height = Node::height(&self.right);
+        self.height = 1 + left_height.max(right_height);
+    }
+
+    fn balance(mut self: NodeRef<V>) -> NodeRef<V> {
+        // 1. Calculate heights
+        self.fix_height();
+
+        // 2. Calculate balance factor
+        let balance_factor = Node::height(&self.left) as i32 - Node::height(&self.right) as i32;
+        println!("Balance = {}", balance_factor);
+
+        // 3. Rebalance if required
+        if balance_factor > 1 {
+            let l = self.left.as_ref().map_or(0, |n| Node::height(&n.left));
+            let r = self.left.as_ref().map_or(0, |n| Node::height(&n.right));
+
+            if l > r {
+                // Left left case
+                self.rotate_right()
+            } else {
+                // Left right case
+                self.left = Some(self.left.expect("Should have left node").rotate_left());
+                self.rotate_right()
             }
-        } else if value > self.value {
-            match &self.right {
-                Some(right_node) => right_node.borrow_mut().insert(value),
-                None => self.right = Some(Box::new(RefCell::new(Node::new(value)))),
+        } else if balance_factor < -1 {
+            let l = self.right.as_ref().map_or(0, |n| Node::height(&n.left));
+            let r = self.right.as_ref().map_or(0, |n| Node::height(&n.right));
+            if r > l {
+                // Right right case
+                self.rotate_left()
+            } else {
+                // Right left case
+                self.right = Some(self.right.expect("Should have right node").rotate_right());
+                self.rotate_left()
             }
         } else {
-            println!("Same value... Ignoring")
+            self
         }
+    }
+
+    fn insert(mut self: NodeRef<V>, value: V) -> NodeRef<V> {
+        if value < self.value {
+            if let Some(left) = self.left {
+                self.left = Some(left.insert(value));
+            } else {
+                self.left = Some(Box::new(Node::new(value)))
+            }
+        } else if value > self.value {
+            if let Some(right) = self.right {
+                self.right = Some(right.insert(value));
+            } else {
+                self.right = Some(Box::new(Node::new(value)))
+            }
+        } else {
+            self.value = value;
+        }
+        self.balance()
     }
 }
 
 #[derive(Debug)]
 struct Tree<V> {
-    root: Option<Node<V>>,
+    root: Option<NodeRef<V>>,
 }
 
 impl<V: std::cmp::PartialOrd> Tree<V> {
@@ -46,23 +131,25 @@ impl<V: std::cmp::PartialOrd> Tree<V> {
     }
 
     fn insert(&mut self, value: V) {
-        match &mut self.root {
-            Some(root) => root.insert(value),
-            None => self.root = Some(Node::new(value)),
+        if let Some(root) = self.root.take() {
+            self.root = Some(root.insert(value));
+        } else {
+            self.root = Some(Box::new(Node::new(value)))
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Node, Tree};
+    use super::Tree;
 
     #[test]
     fn create_nodes() {
         let mut tree = Tree::<i32>::new();
         tree.insert(1);
-        tree.insert(2);
         tree.insert(3);
+        tree.insert(2);
+
         println!("{:#?}", tree);
     }
 }
